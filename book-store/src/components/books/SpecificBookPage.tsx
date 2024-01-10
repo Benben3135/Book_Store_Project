d70a22afa9c2ce774392083e1a80c76960c1a5f9
itahTamar <tamar.verber@gmail.com>
2024-01-10 16:59:20 +0200

    create client router

---
+++ book-store/src/components/books/SpecificBookPage.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import { Book } from './BookCard';
+
+const SpecificBookPage = () => {
+    const [bookData, setBookData] = useState<Book>();
+    
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SpecificBookPage
