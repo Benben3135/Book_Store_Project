a68b54a295be03d53835c5694e098ca31f01945f
itahTamar <tamar.verber@gmail.com>
2024-01-11 21:19:19 +0200

    update the book list book.ts

--- book-store/src/util/books.ts
+++ book-store/src/util/books.ts
@@ -48,7 +48,137 @@ export const books:Book[] = [
         publisher: "Gramedia Pustaka Utama",
         description: "Bila kau mencintai orang yang akan membunuhmu, kau tak punya pilihan. Bila nyawamu satu-satunya yang harus kauberikan untuk orang yang kaucintai, bagaimana mungkin kau tidak memberikannya? Bagi Bella Swan, mencintai dan dicintai vampir bernama Edward adalah bagaikan khayalan dan mimpi buruk yang dirajut jadi satu ke dalam kenyataan. Bukan itu saja, hubungannya yang sangat istimewa dengan Jacob Black sang werewolf, ternyata menyeret Bella ke pilihan-pilihan pelik yang membuat hati keduanya tercabik-cabik. Tapi konon cinta harus memilih, dan karenanya Bella harus memutuskan. Dan sebagai orang yang sangat mengenal Bella, Jacob tahu persis apa keputusan gadis itu. Lalu sanggupkah Jacob meninggalkan Bella selamanya untuk menyembuhkan lukaluka hatinya sendiri? Dan ketika Bella membutuhkannya, sanggupkah Jacob mengatakan tidak?",
         image: "https://img1.od-cdn.com/ImageType-400/1191-1/C91/930/8E/%7BC919308E-2BB5-4F24-80E8-BB7907A69DE2%7DImg400.jpg",
-        likes: 9,
+        likes: 7,
         genre: "Romance"
     },
+    {
+        title: "Ender's Game",
+        author: "Orson Scott Card",
+        pageNum: 324,
+        publisher: "Tor Publishing Group",
+        description: "In order to develop a secure defense against a hostile alien race's next attack, government agencies breed child geniuses and train them as soldiers. A brilliant young boy, Andrew 'Ender' Wiggin lives with his kind but distant parents, his sadistic brother Peter, and the person he loves more than anyone else, his sister Valentine. Peter and Valentine were candidates for the soldier-training program but didn't make the cut—young Ender is the Wiggin drafted to the orbiting Battle School for rigorous military t...",
+        image: "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/d20ab9dbbe936aacfa999dd5d331b9efa34ad1212ae420e52e8eb0d7dab8e882._RI_TTW_.jpg",
+        likes: 8,
+        genre: "Sci-fiction"
+    },
+    {
+        title: "Speaker for the Dead",
+        author: "Orson Scott Card",
+        pageNum: 415,
+        publisher: "Tor Publishing Group",
+        description: "Ender Wiggin, the hero and scapegoat of mass alien destruction in Ender's Game, receives a chance at redemption in this novel. Ender, who proclaimed as a mistake his success in wiping out an alien race, wins the opportunity to cope better with a second race, discovered by Portuguese colonists on the planet Lusitania. Orson Scott Card infuses this long, ambitious tale with intellect by casting his characters in social, religious and cultural contexts.",
+        image: "https://cdn.kobo.com/book-images/41544f94-2b8c-4859-aa8b-a417d42c958c/1200/1200/False/speaker-for-the-dead-1.jpg",
+        likes: 9,
+        genre: "Sci-fiction"
+    },
+    {
+        title: "Frozen (Disney Frozen)",
+        author: "Victoria Saxon",
+        pageNum: 24,
+        publisher: "Random House Children's Books",
+        description: "Walt Disney Animation Studios presents Frozen, a stunning big-screen comedy adventure. Fearless optimist Anna sets off on an epic journey—teaming up with rugged mountain man Kristoff and his loyal reindeer Sven—to find her sister Elsa, whose icy powers have trapped the kingdom of Arendelle in eternal winter. Encountering Everest-like conditions, mystical trolls and a hilarious snowman named Olaf, Anna, and Kristoff battle the elements in a race to save the kingdom. Children ages 2-5 will enjoy this Little Golden Book retelling the whole exciting story.",
+        image: "https://lumiere-a.akamaihd.net/v1/images/p_frozen_18373_3131259c.jpeg?region=0%2C0%2C540%2C810",
+        likes: 6,
+        genre: "Children"
+    },
+    {
+        title: "The Little Mermaid",
+        author: "Sue DiCicco",
+        pageNum: 22,
+        publisher: "Random House Children's Books",
+        description: "Disney isney's classic animated film 'The Little Mermaid' is retold in a sturdy, oversized board book. With big pages and beautiful illustrations, it's perfect for reading aloud to the youngest Disney Princess fans.",
+        image: "https://lumiere-a.akamaihd.net/v1/images/p_thelittlemermaid_6a6ef760.jpeg",
+        likes: 7,
+        genre: "Children"
+    },
+    {
+        title: "If It Bleeds",
+        author: "Stephen King",
+        pageNum: 623,
+        publisher: "LARGE PRINT DISTRIBUTION",
+        description: "From the legendary storyteller and master of short fiction Stephen King comes an extraordinary collection of four new 'exceptionally compelling novellas that reaffirm [King's] mastery of the form' (The Washington Post). Readers adore Stephen King's novels, and his novellas are their own dark treat, briefer but just as impactful and enduring as his longer fiction. Many of his novellas have been made into iconic films, including 'The Body' (Stand By Me) and 'Rita Hayworth and Shawshank Redemption' (Shawshank Redemption). Four brilliant new tales in If It Bleeds are sure to prove as iconic as their predecessors. Once again, King's remarkable range is on full display. In the title story, reader favorite Holly Gibney (from the Mr. Mercedes tril...",
+        image: "https://upload.wikimedia.org/wikipedia/en/2/24/If_It_Bleeds_%28Stephen_King%29.png",
+        likes: 9,
+        genre: "Horror"
+    },
+    {
+        title: "Cat's Eyewitness",
+        author: "Rita Mae Brown",
+        pageNum: 463,
+        publisher: "Large Print Press",
+        description: "From the legendary storyteller and master of short fiction Stephen King comes an extraordinary collection of four new 'exceptionally compelling novellas that reaffirm [King's] mastery of the form' (The Washington Post). Readers adore Stephen King's novels, and his novellas are their own dark treat, briefer but just as impactful and enduring as his longer fiction. Many of his novellas have been made into iconic films, including 'The Body' (Stand By Me) and 'Rita Hayworth and Shawshank Redemption' (Shawshank Redemption). Four brilliant new tales in If It Bleeds are sure to prove as iconic as their predecessors. Once again, King's remarkable range is on full display. In the title story, reader favorite Holly Gibney (from the Mr. Mercedes tril...",
+        image: "https://upload.wikimedia.org/wikipedia/en/2/24/If_It_Bleeds_%28Stephen_King%29.png",
+        likes: 5,
+        genre: "Mystery"
+    },
+    {
+        title: "Cat of the Century",
+        author: "Rita Mae Brown",
+        pageNum: 219,
+        publisher: "Bantam Books",
+        description: "Acclaimed authors Rita Mae Brown and her feline partner, Sneaky Pie Brown, are back with this new mystery starring Mary Minor ,Harry, Haristeen, the sleuthing cats Mrs. Murphy and Pewter, and corgi Tee Tucker. And this time they must catch a killer determined to turn a birthday party into a funeral. Harry's beloved and tart-tongued Aunt Tally is about to turn the big 1-0-0. The alumnae association of her alma mater sees an opportunity to honor the event and make some loot off the centennial as well. The plan is to hold a big fund-raiser in Aunt Tally's honor to recoup some of the school revenue lost in the cratered economy. But soon there's more at risk than investments and endowments.First, an impending blizzard threatens to ruin the whole affair. Then a suspicious transact...",
+        image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1435613827l/6707234.jpg",
+        likes: 5,
+        genre: "Mystery"
+    },
+    {
+        title: "Harry Potter and the Sorcerer's Stone",
+        author: "J. K. Rowling",
+        pageNum: 320,
+        publisher: "Scholastic Incorporated",
+        description: "Harry Potter has never been the star of a Quidditch team, scoring points while riding a broom far above the ground. He knows no spells, has never helped to hatch a dragon, and has never worn a cloak of invisibility. All he knows is a miserable life with the Dursleys, his horrible aunt and uncle, and their abominable son, Dudley - a great big swollen spoiled bully. Harry's room is a tiny closet at the foot of the stairs, and he hasn't had a birthday party in eleven years. But all that is about to change when a mysterious letter arrives by owl messenger: a letter with an invitation to an incredible place that Harry - and anyone who reads about him - will find unforgettable.",
+        image: "https://m.media-amazon.com/images/I/71RVt35ZAbL._AC_UF1000,1000_QL80_.jpg",
+        likes: 7,
+        genre: "Fantasy"
+    },
+    {
+        title: "Harry Potter and the Chamber of Secrets",
+        author: "J. K. Rowling",
+        pageNum: 251,
+        publisher: "Scholastic Canada Limited",
+        description: "Harry Potter and his friends return for their second year at Hogwarts.",
+        image: "https://prodimage.images-bn.com/pimages/9780439064873_p0_v1_s1200x630.jpg",
+        likes: 7,
+        genre: "Fantasy"
+    },
+    {
+        title: "Elon Musk Biography",
+        author: "Alexander Cooper",
+        pageNum: 0,
+        publisher: "Ben Business Group LLC",
+        description: "Elon Musk Biography, A Fascinating Life Story. When people today talk about modern-day success stories in entrepreneurship, engineering, and technological innovation, Elon Musk's name inevitably garners much of the discussion. Musk is the name behind some of the most recognizable, trailblazing, and talked-about companies this century. He is constantly ranked among the wealthiest business magnates in the world whilst also maintaining a persona akin to a rock star which only serves to contribute to his popularity. A look at the background and beginnings of Elon Reeve Musk reveals where he gets much of his innovative and exploring spirit. His father, Errol Musk, is an electromechanical engineer, sailor and pilot. Meanwhile, his mother Maye is a model and a dietician. Musk's fath...",
+        image: "https://cdn2.penguin.com.au/covers/original/9780753555644.jpg",
+        likes: 3,
+        genre: "Biography"
+    },
+    {
+        title: "The Order",
+        author: "Daniel Silva",
+        pageNum: 560,
+        publisher: "HarperCollins Publishers",
+        description: "It was nearly one a.m. by the time he crawled into bed. Chiara was reading a novel, oblivious to the television, which was muted. On the screen was a live shot of St. Peter's Basilica. Gabriel raised the volume and learned that an old friend had died...Gabriel Allon has slipped quietly into Venice for a much-needed holiday with his wife and two young children. But when Pope Paul VII dies suddenly, Gabriel is summoned to Rome by the Holy Father's loyal private secretary, Archbishop Luigi Donati. A billion Catholic faithful have ....",
+        image: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1595570988l/48676759.jpg",
+        likes: 8,
+        genre: "Thriller"
+    },
+    {
+        title: "The Fallen Angel",
+        author: "Daniel Silva",
+        pageNum: 405,
+        publisher: "HarperCollins Publishers",
+        description: " 'Allon is the 21st century Bond' Daily Mail Bruised and war-weary following his secret war to bring down a terrorist mastermind, Gabriel Allon returns to his beloved Rome to restore a Caravaggio masterpiece. But early one morning Gabriel is summoned by his friend and occasional ally Monsignor Luigi Donati, the all-powerful private secretary to the Pope. The broken body of a beautiful woman lies beneath Michelangelo's magnificent dome. Donati fears a public inquiry will inflict more wounds on an already-damaged Church so he calls upon Gabriel to use his matchless talents and experience to quietly pursue the truth - was it suicide, or something more sinister?",
+        image: "https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1468408514i/13390528.jpg",
+        likes: 3,
+        genre: "Thriller"
+    },
+    {
+        title: "Blanche: The Maid of Lille",
+        author: "Ossip Schubin",
+        pageNum: 23,
+        publisher: "",
+        description: "",
+        image: "https://m.media-amazon.com/images/I/416v0HupVTL.jpg",
+        likes: 8,
+        genre: "Fiction"
+    }, 
 ]
